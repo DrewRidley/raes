6d57e0909825782e0f3fff0d85e07b8208bc59ac
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-04-08 14:49:33 -0400

    Wrote template code for decryption

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -1,1 +1,19 @@
+// This file holds the code to decrypt using our implementation of the AES algorithm. 
 
+use std::fs::File;
+
+pub fn decrypt<const N: usize>(ciphertext: File, key: &[u8; N]) -> File {
+    let round_keys = create_round_keys(*key);
+    perform_rounds(round_keys);
+}
+
+fn create_round_keys<const N: usize>(key: [u8; N]) -> Vec<[u8; N]> {
+    
+}
+
+fn perform_rounds<const N: usize>(round_keys: Vec<[u8; N]>) {
+    add_round_key();
+    inverse_mix_columns();
+    shift_rows();
+    inverse_sub_byte();
+}
