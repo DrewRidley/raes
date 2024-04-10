f50bfb3a45700ca857d4d111100c8de3319f6d5a
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-04-10 16:55:09 -0400

    fixed key expansion to follow the pseudocode in the NIST spec

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -2,88 +2,72 @@
 
 use std::fs::File;
 
-pub fn decrypt<const N: usize>(ciphertext: File, key: &[u32; N], path: String) -> File {
-    let round_keys = create_round_keys(*key, 11);
-    perform_rounds::<N>(round_keys);
+pub fn decrypt(ciphertext: File, key: &[u8; 32], path: String) -> File {
+    let round_keys = key_expansion(*key);
+    perform_rounds(&round_keys);
     File::create(path).unwrap()
 }
 
+
+// AES-256: key length = 8 words
+// block size = 4 words
+// 14 rounds
+
 // this function will perform the operations of a decryption round
-fn perform_rounds<const N: usize>(round_keys: Vec<u32>) {
-    add_round_key();
-    inverse_mix_columns();
-    shift_rows();
-    inverse_sub_byte();
+fn perform_rounds(round_keys: &[u32; 60]) {
+
+    add_round_key(round_keys[round_keys.len()-1]); // add last round key
+
+    for i in (1..14).rev() { // reversed so round keys are inserted in reverse
+        inverse_shift_rows();
+        inverse_sub_bytes();
+        add_round_key(round_keys[i]);
+        inverse_mix_columns();
+    }
+
+    inverse_shift_rows();
+    inverse_sub_bytes();
+    add_round_key(round_keys[0]);
 }
 
-// generate 128-bit round keys for each round plus one more
-// key in AES-128 is four 32-bit (4-byte) words
-// eleven round keys for AES-128
+// generate 14 4-byte round keys for each round plus one more
+// 60 bytes total
 
-// rcon_i is the round constant for round i
-// rcon_i = [rc_i 0x00 0x00 0x00]
-// where rc_i is byte defined as:
-// rc_i = 1                             if i = 1
-//        2 * rc_{i-1}                  if i > 1 AND rc_{i-1} < 0x80
-//        (2 * rc_{i-1}) ^ 0x11B        if i > 1 and rc_{i-1} >= 0x80
+fn key_expansion(key: [u8; 32]) -> [u32; 60] {
 
-// RotWord([b0, b1, b2, b3]) = [b1, b2, b3, b0]
-// SubWord([b0, b1, b2, b3]) = [S(b0), S(b1), S(b2), S(b3)]
-// W_0 .. W_{4R-1} are the 32-bit words of the expanded key
+    let mut w: [u32; 60] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-// for i = 0 .. 4R-1
-// W_i =    K                                                   if i < N
-//          W_{i-N} ^ SubWord(RotWord(W_{i-1})) ^ rcon_{i/N}    if i >= N AND i%N == 0
-//          W_{i-N} ^ SubWord(W_{i-1})                          if (i >= N AND N > 6) AND (i%N == 4)
-//          W_{i-N} ^ W_{i-1}                                   otherwise
+    let mut temp: u32 = 0;
 
-// this could be split up into functions or made recursive I think.
-fn create_round_keys<const N: usize>(key: [u32; N], number_of_rounds: u32) -> Vec<u32> {
-    let mut round_keys = Vec::new();
-    let mut round_constants: Vec<u32> = Vec::new();
-    let mut round_number = 1;
+    let mut i = 0;
 
-    let mut rc_list = Vec::new();
+    const round_constants: [u32; 10] = [0x01 << 24, 0x02 << 24, 0x04 << 24, 0x08 << 24, 0x10 << 24, 0x20 << 24, 0x40 << 24, 0x80 << 24, 0x1B << 24, 0x36 << 24];
 
-    let rc: u8 = {
-        let last_rc = rc_list[round_number - 1];
-        if round_number == 1 {
-            1
-        } else if round_number > 1 && last_rc < 0x80 {
-            2 * last_rc
-        } else if round_number > 1 && last_rc >= 0x80 {
-            (2 * last_rc) ^ 0x11
-        } else {
-            todo!()
+    while i < 8 {
+        w[i] = u8s_to_u32([key[4*i], key[4*i+1], key[4*i+2], key[4*i+3]]);
+        i = i + 1;
+    }
+
+    i = 8;
+
+    while i < 60 {
+        temp = w[i-1];
+
+        if i % 8 == 0 {
+            temp = sub_word(rot_word(temp)) ^ round_constants[i/8];
+        } 
+        else if i % 8 == 4 {
+            temp = sub_word(temp);
         }
-    };
 
-    rc_list.push(rc);
+        w[i] = w[i-8] ^ temp;
+        i = i + 1;
+    }
+    w
+ }
 
-    round_constants.push((rc as u32) << 24);
-
-    let words = Vec::new();
-
-    let w_i: u32 = {
-        let last_word = words[round_number - 1];
-        let inv_word = words[round_number - N];
-
-        // Bit shifting isn't implemented for [u8], maybe there's a better way to store the words?
-        if round_number < N {
-            key[round_number] // figure this out
-        } else if round_number >= N && (round_number % N) == 0 {
-            inv_word ^ sub_word(rot_word(last_word) ^ round_constants[round_number / N])
-        } else if round_number >= N && N > 6 && (round_number % N) == 4 {
-            inv_word ^ sub_word(last_word)
-        } else {
-            inv_word ^last_word
-        }
-    };
-
-    round_keys.push(w_i);
-    round_number += 1;
-
-    round_keys
+fn u8s_to_u32(bytes: [u8; 4]) -> u32 {
+    (bytes[0] as u32) << 24 | (bytes[1] as u32) << 16 | (bytes[2] as u32) << 8 | (bytes[3] as u32)
 }
 
 fn sub_word(word: u32) -> u32 {
@@ -92,19 +76,20 @@ fn sub_word(word: u32) -> u32 {
     0
 }
 
-fn rot_word(mut word: u32) -> u32{
-    let first_byte = (word >> 24) & 0xFF ;
+fn rot_word(mut word: u32) -> u32 {
+    let first_byte = (word >> 24) & 0xFF;
 
+    // shift the first byte out and add it to the end
     (word << 8) | first_byte
 }
 
-fn add_round_key(){}
+fn add_round_key(round_key: u32) {}
 
-fn inverse_mix_columns(){}
+fn inverse_mix_columns() {}
 
-fn shift_rows(){}
+fn inverse_shift_rows() {}
 
-fn inverse_sub_byte(){}
+fn inverse_sub_bytes() {}
 
 #[cfg(test)]
 mod test {
