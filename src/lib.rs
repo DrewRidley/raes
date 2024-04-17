2cc498d01141914dc0790ade5830a8c64e4d445c
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-04-17 14:23:57 -0400

    added MixColumns and test to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,16 +11,13 @@ pub mod shared {
 
     pub fn key_expansion(key: [u8; 32]) -> [u32; 8] {
         let mut w: [u32; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
-
         let mut temp: u32 = 0;
-
         let mut i = 0;
 
         while i < 8 {
             w[i] = u8s_to_u32([key[4 * i], key[4 * i + 1], key[4 * i + 2], key[4 * i + 3]]);
             i = i + 1;
         }
-
         i = 8;
 
         while i < 8 {
@@ -51,9 +48,6 @@ pub mod shared {
         let b1 = (word >> 8 & 0xFF) as u8;
         let b0 = (word & 0xFF) as u8;
 
-        // println!("b0: {:x}, \nb1: {:x}, \nb2: {:x}, \nb3: {:x}", b0, b1, b1, b0);
-        // println!("sub_bytes of {:x}: {:x}", b0, sub_bytes(b0));
-
         let s3 = sub_bytes(b3) as u32;
         let s2 = sub_bytes(b2) as u32;
         let s1 = sub_bytes(b1) as u32;
@@ -74,6 +68,31 @@ pub mod shared {
 
         // shift the first byte out and add it to the end
         (word << 8) | first_byte
+    }
+
+    fn mix_columns(state: [[u8; 4]; 4]) -> [[u8; 4]; 4] {
+        let mut output = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+
+        for j in 0..4 {
+            let mut column = state[j];
+            let mut copy: [u8; 4] = [0, 0, 0, 0];
+            let mut doubled_copy: [u8; 4] = [0, 0, 0, 0];
+            let mut high_bit: u8 = 0;
+
+            for i in 0..4 {
+                copy[i] = column[i];
+
+                high_bit = column[i] >> 7;
+                doubled_copy[i] = column[i] << 1;
+                doubled_copy[i] ^= high_bit * 0x1B;
+            }
+            column[0] = doubled_copy[0] ^ copy[3] ^ copy[2] ^ doubled_copy[1] ^ copy[1];
+            column[1] = doubled_copy[1] ^ copy[0] ^ copy[3] ^ doubled_copy[2] ^ copy[2];
+            column[2] = doubled_copy[2] ^ copy[1] ^ copy[0] ^ doubled_copy[3] ^ copy[3];
+            column[3] = doubled_copy[3] ^ copy[2] ^ copy[1] ^ doubled_copy[0] ^ copy[0];
+            output[j] = column;
+        }
+        output
     }
 
     #[cfg(test)]
@@ -132,6 +151,25 @@ pub mod shared {
             let expected_output = 0x12_34_56_78;
 
             assert_eq!(output, expected_output);
+        }
+
+        #[test]
+        fn test_mix_columns() {
+            let state = [
+                [0x63, 0x53, 0xe0, 0x8c],
+                [0x09, 0x60, 0xe1, 0x04],
+                [0xcd, 0x70, 0xb7, 0x51],
+                [0xba, 0xca, 0xd0, 0xe7],
+            ];
+            let expected = [
+                [0x5f, 0x72, 0x64, 0x15],
+                [0x57, 0xf5, 0xbc, 0x92],
+                [0xf7, 0xbe, 0x3b, 0x29],
+                [0x1d, 0xb9, 0xf9, 0x1a],
+            ];
+
+            let output = mix_columns(state);
+            assert_eq!(expected, output);
         }
     }
 }
