b8d712d82f49ffecbe4dd260651b6a795059f5a6
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-04-26 15:58:17 -0400

    fixed key generation

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,8 @@ pub mod shared {
     use crate::constant::INVERSE_SBOX;
     pub use crate::constant::{ROUND_CONSTANTS, SBOX};
 
-    pub fn key_expansion(key: [u8; 32]) -> [u32; 8] {
-        let mut w: [u32; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+    pub fn key_expansion(key: [u8; 32]) -> [u32; 60] {
+        let mut w: [u32; 60] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         let mut temp: u32 = 0;
         let mut i = 0;
 
@@ -19,7 +19,7 @@ pub mod shared {
             w[i] = u8s_to_u32([key[4 * i], key[4 * i + 1], key[4 * i + 2], key[4 * i + 3]]);
             i = i + 1;
         }
-        i = 8;
+        assert!(i == 8);
 
         while i < 8 {
             temp = w[i - 1];
@@ -225,8 +225,8 @@ pub mod shared {
             let expanded_keys = key_expansion(test_key);
 
             let expected_output = [
-                0x603deb10, 0x15ca71be, 0x2b73aef0, 0x857d7781, 0x1f352c07, 0x3b6108d7, 0x2d9810a3,
-                0x0914dff4,
+                0x603deb10, 0x15ca71be, 0x2b73aef0, 0x857d7781, 0x1f352c07, 0x3b6108d7, 0x2d9810a3, 0x0914dff4, 
+                0x9ba35411, 0x8e6925af, 0xa51a8b5f, 0x2067fcde, 0xa8b09c1a, 0x93d194cd, 0xbe49846e, 0xb75d5b9a, 0xd59aecb8, 0x5bf3c917, 0xfee94248, 0xde8ebe96, 0xb5a9328a, 0x2678a647, 0x98312229, 0x2f6c79b3, 0x812c81ad, 0xdadf48ba, 0x24360af2, 0xfab8b464, 0x98c5bfc9, 0xbebd198e, 0x268c3ba7, 0x09e04214, 0x68007bac, 0xb2df3316, 0x96e939e4, 0x6c518d80, 0xc814e204, 0x76a9fb8a, 0x5025c02d, 0x59c58239, 0xde136967, 0x6ccc5a71, 0xfa256395, 0x9674ee15, 0x5886ca5d, 0x2e2f31d7, 0x7e0af1fa, 0x27cf73c3, 0x749c47ab, 0x18501dda, 0xe2757e4f, 0x7401905a, 0xcafaaae3, 0xe4d59b34, 0x9adf6ace, 0xbd10190d, 0xfe4890d1, 0xe6188d0b, 0x046df344, 0x706c631e
             ];
 
             println!("expanded key: {:x?}", expanded_keys);
